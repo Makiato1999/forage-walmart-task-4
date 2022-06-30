b939c66235da12a5ffd51d211c94bc111672fba1
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2022-06-30 18:14:42 -0500

    Updated
    csv0

--- script.py
+++ script.py
@@ -11,7 +11,6 @@ print("Success to connect "+db_name+"\n")
 with open('data/shipping_data_0.csv', 'r')as csv_file_0:
     csv_reader_0 = csv.reader(csv_file_0)
     next(csv_reader_0)
-    product_id = 0
     product_name = ''
     quantity = ''
     origin = ''
@@ -19,16 +18,27 @@ with open('data/shipping_data_0.csv', 'r')as csv_file_0:
 
     for row in csv_reader_0:
         product_name = row[2]
+
+        insertQuery_product = f"INSERT OR IGNORE INTO {product_table_name} VALUES (NULL,'{product_name}')"
+        cur.execute(insertQuery_product)
+    print(f"Success to transfer data to {product_table_name} table\n")
+
+    csv_file_0.seek(0)
+    csv_reader_0 = csv.reader(csv_file_0)
+    next(csv_reader_0)
+    for row in csv_reader_0:
+        product_name = row[2]
         quantity = row[4]
         origin = row[0]
         destination = row[1]
 
-        insertQuery_product = f"INSERT OR IGNORE INTO {product_table_name} VALUES ('{product_id}','{product_name}')"
-        #insertQuery_shipment = f"INSERT INTO shipment VALUES ('{}', '{product_id}', '{quantity}', '{origin}', '{destination}')"
-        
-        cur.execute(insertQuery_product)
-        con.commit()
-        product_id += 1
+        selectQuery_product = f"SELECT id FROM {product_table_name} WHERE name = '{product_name}'"
+        cur.execute(selectQuery_product)
+        res = cur.fetchone()
+
+        insertQuery_shipment = f"INSERT INTO {shipment_table_name} VALUES (NULL, '{res[0]}', '{quantity}', '{origin}', '{destination}')"
+        cur.execute(insertQuery_shipment)
+    print(f"Success to transfer data to {shipment_table_name} table\n")
+    con.commit()
 cur.close()
 con.close()
-print("Success to transfer data\n")
